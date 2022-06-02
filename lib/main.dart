8a11d62970a808a40b912504239e71e09f02d013
Manuele Mion <manuele.mion@gmail.com>
2022-06-02 17:26:19 +0200

    Added the weekly planner view

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meal_planner/calendar_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalendarList(),
+    );
+  }
+}
